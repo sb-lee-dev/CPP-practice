b47f2f24c4c9bf3631aa78613394b104c92f8e07
seongbaelee <slee617seneca@gmail.com>
2026-01-02 17:34:50 -0500

    simple practice with math functions

--- c++_project/helloworld.cpp
+++ c++_project/helloworld.cpp
@@ -3,19 +3,18 @@
 
 int main() {
 
-	double x = 3.99;
-	double y = 4;
-	double z;
+	double a;
+	double b;
+	double c;
 
-	//z = std::max(x,y);
-	//z = std::min(x, y);
-	//z = pow(2,4);
-	//z = sqrt(9);
-	//z = abs(-3);
-	//z = round(x);
-	//z = ceil(x);
-	z = floor(x);
-	std::cout << z;
+	std::cout << "Enter side A: ";
+	std::cin >> a;
 
+	std::cout << "Enter side B: ";
+	std::cin >> b;
+
+	c = sqrt(pow(a, 2) + pow(b, 2));
+
+	std::cout << "side C: " << c;
 	return 0;
 }
