8246b2bad3c481f0fa6189eb533528cde1b9796b
seongbaelee <slee617seneca@gmail.com>
2026-01-02 17:12:07 -0500

    basic arithmetic operators

--- c++_project/helloworld.cpp
+++ c++_project/helloworld.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 #include <vector>
 
-//typedef std::vector<std::pair<std::string, int>> pairlist_t;
-//typedef std::string text_t;
-//typedef int number_t;
-
-using text_t = std::string;
-using number_t = int;
-
 int main() {
 
-	text_t firstName = "Bro";
-	number_t age = 21;
+	int students = 20;
+	//students = students + 2;
+	//students+=1;
+	//students++;
 
-	std::cout << firstName << "\n";
-	std::cout << age << "\n";
+	//students = students - 1;
+	//students -= 1;
+	//students--;
+
+	//students = students * 2;
+	//students *= 2;
+
+	//students = students / 2;
+	//students /= 3;
+
+	int remainder = students % 3;
+
+	int calc = 6 - (5 + 4) * 3 / 2;
+	std::cout << calc;
 
 	return 0;
 }
