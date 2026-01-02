de7c138d350790845823ccffeb73e32483377f60
seongbaelee <slee617seneca@gmail.com>
2026-01-02 16:15:04 -0500

    use const to make variables read only

--- c++_project/helloworld.cpp
+++ c++_project/helloworld.cpp
@@ -2,52 +2,10 @@
 
 int main() {
 	
-	int x; //declaration
-	x = 5; //assignment
-
-	int y = 6;
-	int sum = x + y;
-
-	std::cout << x << '\n';
-	std::cout << y << '\n';
-	std::cout << sum << '\n';
-
-	//integer (whole number)
-	int age = 21;
-	int year = 2026;
-	int days = 7;
-
-	std::cout << days << '\n';
-
-	//double (number including decimal)
-	double price = 10.99;
-	double gpa = 2.5;
-	double temp = 25.1;
-
-	std::cout << price << '\n';
-
-	// single character
-	char grade = 'A';
-	char initial = 'C';
-	char currency = '$';
-
-	std::cout << currency << '\n';
-
-	// boolean (true or false)
-	bool student = true;
-	bool power = false;
-	bool forSale = true;
-
-	std::cout << power << '\n';
-
-	// string (objects that represents a sequence of text)
-	std::string name = "Bro";
-	std::string day = "Friday";
-	std::string food = "pizza";
-	std::string address = "123 Fake St.";
-
-	std::cout << "Hello " << name << '\n';
-	std::cout << "You are " << age << " years old";
+	const double PI = 3.14159;
+	const int LIGHT_SPEED = 299792458;
+	const int WIDTH = 1920;
+	const int HEIGHT = 1080;
 
 	return 0;
 }
