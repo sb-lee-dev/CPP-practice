dcff94c1880896d881092108adc9838c82ac60ed
seongbaelee <slee617seneca@gmail.com>
2026-01-03 12:10:44 -0500

    array in for loop

--- c++_project/helloworld.cpp
+++ c++_project/helloworld.cpp
@@ -4,14 +4,13 @@
 #include <ctime>
 
 int main() {
-	std::string name = "Bro hihi";
-	double gpa = 2.5;
-	char grade = 'A';
-	bool student = true;
-	char grades[] = {'A', 'B', 'C', 'D', 'F'};
-	std::string students[] = { "Spongebob", "Patrick", "Squidward" };
+	
+	//std::string students[] = { "Spongebab", "Patrick", "Squidward", "Sandy" };
+	char grades[] = { 'A', 'B', 'C', 'D', 'F' };
 
-	std::cout << sizeof(students) / sizeof (std::string) << "elements\n";
+	for (int i = 0; i < sizeof(grades) / sizeof(char); i++) {
+		std::cout << grades[i] << '\n';
+	}
 
 	return 0;
 }
