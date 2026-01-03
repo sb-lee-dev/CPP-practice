1095357827de30428e87d28347068c9ebf9da06b
seongbaelee <slee617seneca@gmail.com>
2026-01-03 18:58:30 -0500

    recursion

--- c++_project/helloworld.cpp
+++ c++_project/helloworld.cpp
@@ -3,37 +3,48 @@
 #include <limits>
 #include <ctime>
 
+//void walk1(int steps);
+//void walk2(int steps);
+int factorial(int num);
+
 int main() {
 
-	/*int* pNum = nullptr;
+	std::cout << factorial(10);
+	//walk2(10);
 
-	pNum = new int;
-
-	*pNum = 21;
-
-	std::cout << "address: " << pNum << '\n';
-	std::cout << "value: " << *pNum << '\n';
-
-	delete pNum;*/
-
-	char* pGrades = NULL;
-	int size;
-
-	std::cout << "how many grades to enter in?: ";
-	std::cin >> size;
-
-	pGrades = new char[size];
-
-	for (int i = 0; i < size; i++) {
-		std::cout << "Enter grade #" << i + 1 << ": ";
-		std::cin >> pGrades[i];
-	}
-
-	for (int i = 0; i < size; i++) {
-		std::cout << pGrades[i] << " ";
-	}
-
-	delete[] pGrades;
 
 	return 0;
 }
+
+//int factorial(int num) {
+//	int result = 1;
+//	for (int i = 1; i <= num; i++) {
+//		result = result * i;
+//	}
+//
+//	return result;
+//};
+
+int factorial(int num) {
+	if (num > 1) {
+		return num * factorial(num - 1);
+	}
+	else {
+		return 1;
+	}
+};
+
+//iterative
+//void walk1(int steps) {
+//	for (int i = 0; i < steps; i++) {
+//		std::cout << "You take a step!\n";
+//	}
+//}
+
+//recursive
+//void walk2(int steps) {
+//	if(steps > 0) {
+//		std::cout << "You take a step!\n";
+//		walk2(steps - 1);
+//	}
+//}
