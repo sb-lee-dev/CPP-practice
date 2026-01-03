8aae466343da6c488023bb2685485a35322baff2
seongbaelee <slee617seneca@gmail.com>
2026-01-03 12:05:58 -0500

    sizeof() operator

--- c++_project/helloworld.cpp
+++ c++_project/helloworld.cpp
@@ -4,23 +4,14 @@
 #include <ctime>
 
 int main() {
-	/*std::string cars[3];
+	std::string name = "Bro hihi";
+	double gpa = 2.5;
+	char grade = 'A';
+	bool student = true;
+	char grades[] = {'A', 'B', 'C', 'D', 'F'};
+	std::string students[] = { "Spongebob", "Patrick", "Squidward" };
 
-	cars[0] = "Camaro";
-	cars[1] = "Mustang";
-	cars[2] = "Camry";
-
-	std::cout << cars[0] << '\n';
-	std::cout << cars[1] << '\n';
-	std::cout << cars[2] << '\n';*/
-
-	double prices[] = {5.00, 7.50, 9.99, 15.00};
-
-	std::cout << prices[0] << '\n';
-	std::cout << prices[1] << '\n';
-	std::cout << prices[2] << '\n';
-	std::cout << prices[3] << '\n';
-
+	std::cout << sizeof(students) / sizeof (std::string) << "elements\n";
 
 	return 0;
 }
