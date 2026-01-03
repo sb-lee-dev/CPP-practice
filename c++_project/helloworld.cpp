09af1c5f6a91774560f1c7838c1a4d19ec78258d
seongbaelee <slee617seneca@gmail.com>
2026-01-02 20:19:01 -0500

    nested loops

--- c++_project/helloworld.cpp
+++ c++_project/helloworld.cpp
@@ -3,13 +3,24 @@
 
 int main() {
 
-	for (int i = 1; i <= 20; i++) {
-		if (i == 13) {
-			//break;
-			continue;
-		}
+	int rows;
+	int cols;
+	char sb;
 
-		std::cout << i << '\n';
+	std::cout << "How mnay rows?: ";
+	std::cin >> rows;
+
+	std::cout << "How mnay columns?: ";
+	std::cin >> cols;
+
+	std::cout << "Enter a symbol to use: ";
+	std::cin >> sb;
+
+	for (int i = 1; i <= rows; i++) {
+		for (int j = 1; j <= cols; j++) {
+			std::cout << sb;
+		}
+		std::cout << '\n';
 	}
 	
 	return 0;
