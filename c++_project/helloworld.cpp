ceb12b4da26fb783d06fd1cb9f51393d4804b550
seongbaelee <slee617seneca@gmail.com>
2026-01-02 20:05:08 -0500

    while loop

--- c++_project/helloworld.cpp
+++ c++_project/helloworld.cpp
@@ -4,37 +4,13 @@
 int main() {
 
 	std::string name;
-	std::cout << "Enter your name: ";
-	std::getline(std::cin, name);
 
-	/*if (name.length() > 12) {
-		std::cout << "Your name can't be over 12 characters";
+	while (name.empty()) {
+		std::cout << "Enter your name: ";
+		std::getline(std::cin, name);
 	}
-	else {
-		std::cout << "welcome " << name;
-	}*/
 
-	/*if (name.empty()) {
-		std::cout << "You didn't enter your name\n";
-	}
-	else {
-		std::cout << "Hello " << name << '\n';
-	}*/
-
-	//name.clear();
-	//std::cout << "Hello " << name;
-
-	/*name.append("@gmail.com");
-	std::cout << "Your username is now " << name;*/
-
-	/*std::cout << name.at(0);*/
-
-	//name.insert(1, "@");
-	//std::cout << name.find(' ');
-
-	name.erase(0, 3);
-	std::cout << name;
-
+	std::cout << "Hello " << name;
 
 	return 0;
 }
