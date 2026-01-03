1913c869f3b818a5a8a0c3e5826ed1cf8c0542e7
seongbaelee <slee617seneca@gmail.com>
2026-01-03 13:01:57 -0500

    Fill an array with user input

--- c++_project/helloworld.cpp
+++ c++_project/helloworld.cpp
@@ -4,16 +4,27 @@
 #include <ctime>
 
 int main() {
-	const int SIZE = 99;
-	std::string foods[SIZE];
-	fill(foods, foods + (SIZE/3), "pizza");
-	fill(foods + (SIZE / 3), foods + (2 * SIZE / 3), "hamburgers");
-	fill(foods + (2 * SIZE / 3), foods + SIZE, "sushi");
+	std::string foods[5];
+	int size = sizeof(foods) / sizeof(foods[0]);
+	std::string temp;
 
-	for (std::string food : foods) {
-		std::cout << food << '\n';
+	for (int i = 0; i < size; i++) {
+		std::cout << "Enter a food you like or 'q' to quit #" << i + 1 << ": ";
+		std::getline(std::cin, temp);
+		if (temp == "q") {
+			break;
+		}
+		else {
+			foods[i] = temp;
+		}
 	}
-	
+
+	std::cout << "You like the following food:\n";
+
+	for (int i = 0; !foods[i].empty(); i++) {
+		std::cout << foods[i] << '\n';
+	}
+
 	return 0;
 }
 
