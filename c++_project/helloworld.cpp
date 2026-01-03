74f934dc0d8c7bcd94f76f6b986f9a8bf4438221
seongbaelee <slee617seneca@gmail.com>
2026-01-03 14:28:31 -0500

    quiz game

--- c++_project/helloworld.cpp
+++ c++_project/helloworld.cpp
@@ -5,21 +5,49 @@
 
 int main() {
 	
-	std::string cars[][3] = { { "Mustang", "Escape", "F-150" },
-	{ "Corvette", "Equinox", "silverado" },
-	{ "Challenger", "Durango", "Ram 1500" },
-	};
+	std::string questions[] = { "1. What year was C++ created?: ", 
+															"2. Who invented C++?: ", 
+															"3. What is the predecessor of C++?: ", 
+															"Is the Earth flat?: "};
 
-	int rows = sizeof(cars) / sizeof(cars[0]);
-	int columns = sizeof(cars[0]) / sizeof(cars[0][0]);
+	std::string options[][4] = { {"A. 1969", "B. 1975", "C. 1985", "D. 1989"},
+														 { "A. Guido van Rossum", "B. Bjarne Stroustrup", "C. John Carmack", "D. Mark Zuckerburg" },
+														 {"A. C", "B. C+", "C. C--", "C. B++"},
+														 {"A. yes", "B. no", "C. sometimes", "D. What's Earth?"} };
 
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < columns; j++) {
+	char answerKey[] = {'C', 'B', 'A', 'B'};
 
-			std::cout << cars[i][j] << " ";
+	int size = sizeof(questions) / sizeof(questions[0]);
+	char guess;
+	int score = 0;
+
+	for (int i = 0; i < size; i++) {
+		std::cout << "********************\n";
+		std::cout << questions[i] << '\n';
+		std::cout << "********************\n";
+
+		for (int j = 0; j < sizeof(options[i]) / sizeof(options[i][0]); j++) {
+			std::cout << options[i][j] << '\n';
 		}
-		std::cout << '\n';
+
+		std::cin >> guess;
+		guess = toupper(guess);
+
+		if (guess == answerKey[i]) {
+			std::cout << "CORRECT\n";
+			score++;
+		}
+		else {
+			std::cout << "WRONG\n";
+			std::cout << "Answer: " << answerKey[i] << '\n';
+		}
 	}
+	std::cout << "********************\n";
+	std::cout << "       RESULTS\n";
+	std::cout << "********************\n";
+	std::cout << "CORRECT GUESSES: " << score << '\n';
+	std::cout << "# of QUESTIONS: " << size << '\n';
+	std::cout << "SCORE: " << (score / (double)size) * 100 << "%";
 
 	return 0;
 }
