c36661b8c2b6e120caca9472c61edc9b0a3c218b
seongbaelee <slee617seneca@gmail.com>
2026-01-03 19:33:17 -0500

    function template

--- c++_project/helloworld.cpp
+++ c++_project/helloworld.cpp
@@ -3,48 +3,16 @@
 #include <limits>
 #include <ctime>
 
-//void walk1(int steps);
-//void walk2(int steps);
-int factorial(int num);
+template <typename T, typename U>
+
+auto max(T x, U y) {
+	return (x > y) ? x : y;
+};
 
 int main() {
 
-	std::cout << factorial(10);
-	//walk2(10);
-
+	std::cout << max(1, 2.1);
 
 	return 0;
 }
 
-//int factorial(int num) {
-//	int result = 1;
-//	for (int i = 1; i <= num; i++) {
-//		result = result * i;
-//	}
-//
-//	return result;
-//};
-
-int factorial(int num) {
-	if (num > 1) {
-		return num * factorial(num - 1);
-	}
-	else {
-		return 1;
-	}
-};
-
-//iterative
-//void walk1(int steps) {
-//	for (int i = 0; i < steps; i++) {
-//		std::cout << "You take a step!\n";
-//	}
-//}
-
-//recursive
-//void walk2(int steps) {
-//	if(steps > 0) {
-//		std::cout << "You take a step!\n";
-//		walk2(steps - 1);
-//	}
-//}
