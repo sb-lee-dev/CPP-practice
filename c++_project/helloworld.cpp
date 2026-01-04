28f2547eaf6baa19b0ec806b4636a940a98189e7
seongbaelee <slee617seneca@gmail.com>
2026-01-03 20:40:56 -0500

    pass structs as arguments

--- c++_project/helloworld.cpp
+++ c++_project/helloworld.cpp
@@ -3,42 +3,44 @@
 #include <limits>
 #include <ctime>
 
-struct Student {
-	std::string name;
-	double gpa;
-	bool enrolled = true;
+
+struct Car {
+	std::string model;
+	int year;
+	std::string color;
 };
+
+void printCar(Car car);
+void paintCar(Car& car, std::string color);
 
 int main() {
 
-	Student student1;
+	Car car1;
+	Car car2;
 
-	student1.name = "Spongebob";
-	student1.gpa = 3.2;
+	car1.model = "Mustang";
+	car1.year = 2023;
+	car1.color = "red";
 
-	std::cout << student1.name << '\n';
-	std::cout << student1.gpa << '\n';
-	std::cout << student1.enrolled << '\n';
+	car2.model = "Corvette";
+	car2.year = 2024;
+	car2.color = "blue";
 
-	Student student2;
+	paintCar(car1, "silver");
+	paintCar(car2, "gold");
 
-	student2.name = "Patrick";
-	student2.gpa = 2.1;
-
-	std::cout << student2.name << '\n';
-	std::cout << student2.gpa << '\n';
-	std::cout << student2.enrolled << '\n';
-
-	Student student3;
-
-	student3.name = "Squidward";
-	student3.gpa = 1.5;
-	student3.enrolled = false;
-
-	std::cout << student3.name << '\n';
-	std::cout << student3.gpa << '\n';
-	std::cout << student3.enrolled << '\n';
+	printCar(car1);
+	printCar(car2);
 
 	return 0;
 }
 
+void printCar(Car car) {
+	std::cout << car.model << '\n';
+	std::cout << car.year << '\n';
+	std::cout << car.color << '\n';
+}
+
+void paintCar(Car &car, std::string color) {
+	car.color = color;
+}
